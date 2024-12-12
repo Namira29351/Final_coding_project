e291e096ee0bc638d69efea38622e9479f91bc09
Student <student@Admins-MacBook-Pro.local>
2024-12-12 17:09:05 -0500

    This is my update code and read me

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Final_coding_project
-This is my transportation analysis code.
+# Project Name: Young Adult Food Identity
+# Description: 
+- This code is used to see how many young adults (age: 18-24) identify as each different food identity like vegeterian and pescaterian. Data like this can be used for people like entrepernuers who want to open a resturaunt and see what kind of foods busy young adults would want to eat. The code includes a way to see the data by itself as well as a visualization of it. 
+# How to run the code:
+- Running the code will present with a list of options. Option 1 allows you to see the data itself while option 2 allows you to see the bar graph that goes with the data. Option 3 allows you exit the program. 
+# Extensions used:
+- Dataset: [text](https://www.kaggle.com/datasets/sonalishanbhag/dietaryhabitssurvey?resource=download)
+- Pandas
+- Matplotlib
+
--- main.py
+++ main.py
@@ -2,17 +2,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load our bus routes data
-# bus_routes = pd.read_csv('data/DDOT_Bus_Routes_-_Current.csv')
-
-# # What's in our data? Let's look!
-# print("First few rows:")
-# print(bus_routes.head(10))
-# print(bus_routes.dtypes)
-
 # MENU_OPTIONS = {
 
-#     '1': 'See which busses run on Sundays',
+#     '0': 'Analyze which meal people think is most important',
+#     '1': 'Analyze young adults different food identitities',
 #     '2': 'Generate Visualization',
 #     '3': 'Exit program'
 
@@ -21,11 +14,52 @@ import matplotlib.pyplot as plt
 def my_data():
     try:
         data = pd.read_csv('data/Dietary Habits Survey Data.csv')
-        print("file loaded ")
-        return data
+        # print("file loaded ")
+        # num_column = len(data.columns)
+        # print(num_column)
+        subset1 = data[['Age', 'What would best describe your diet:']]
+        # num_column = len(subset1.columns)
+        # print(num_column)
+        return subset1
     except FileNotFoundError:
         print("File has not been uploaded")
         return None
-my_data()
+# my_data()
 data = pd.read_csv('data/Dietary Habits Survey Data.csv')
 
+def main():
+    while True:
+        print("1. Analyze young adults' different food identitities.")
+        print("2. Generate Visualization")
+        print("3. Exit Program")
+
+        user_input = input("What would you like to do? (1-3): ")
+
+        if user_input == "1":
+            young_adult_identity()
+        elif user_input == "2":
+            visualization()
+        elif user_input == "3":
+            print("you are leaving")
+            break
+
+def young_adult_identity():
+    filtered_data = my_data().loc[data['Age'] == '18-24']
+    filtered_data = filtered_data[['What would best describe your diet:']]
+    
+    counts = filtered_data['What would best describe your diet:'].value_counts()
+    print(counts)
+
+
+def visualization():
+    filtered_data = my_data().loc[data['Age'] == '18-24']
+    filtered_data = filtered_data[['What would best describe your diet:']]
+    
+    counts = filtered_data['What would best describe your diet:'].value_counts()
+    counts.plot(kind='bar')
+    plt.title('Young adult food identities')
+    plt.xlabel('Food identies')
+    plt.ylabel('Value')
+    plt.show()
+
+main()
